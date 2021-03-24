6a0f3c5fe4e74b6d63f6901f533cede3c3f4119e
hong7752 <70906642+hong7752@users.noreply.github.com>
2021-03-24 16:18:28 +0800

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,18 @@
+//圖片輪播
+$(document).ready(function () {
+    $('#carouselExampleIndicators').carousel({ interval: 4000 });
+});
+
+//進度條
+window.addEventListener('scroll', () => {
+    moveBar('#e74c3c')
+})
+function moveBar(color = '#0984e3') {
+    let bar = document.querySelector('.bar')
+    let w =
+        Math.ceil((Math.round(window.pageYOffset) /
+            (document.body.scrollHeight - window.innerHeight)) *
+            100)
+    bar.style.width = w + '%'
+    bar.style.background = color
+}
